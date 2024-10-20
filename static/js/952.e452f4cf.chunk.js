3f4cc9062e3105bcf2b0363f179e8257a24ca419
Yannick Rast <92748060+yannickrast@users.noreply.github.com>
2024-10-20 22:48:23 +0200

    Add files via upload

---
+++ static/js/952.e452f4cf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkroadmap_app=self.webpackChunkroadmap_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.e452f4cf.chunk.js.map
